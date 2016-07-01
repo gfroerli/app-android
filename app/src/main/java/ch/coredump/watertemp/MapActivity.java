735e90d802cc56b85ccfa2a99aa002be40f3ec36
Danilo Bargen <mail@dbrgn.ch>
2016-07-01 22:02:58 +0200

    Use new MapboxAccountManager

--- app/src/main/java/ch/coredump/watertemp/MapActivity.java
+++ app/src/main/java/ch/coredump/watertemp/MapActivity.java
@@ -3,6 +3,8 @@ package ch.coredump.watertemp;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+
 public class MapActivity extends Activity {
 
     private static final String TAG = "MapActivity";
@@ -10,6 +12,11 @@ public class MapActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Start mapbox account manager
+        MapboxAccountManager.start(this.getApplicationContext(), getString(R.string.mapbox_access_token));
+
         setContentView(R.layout.activity_map);
     }
+
 }
