037badbd27a7fcd21c700ee9dff00b37b77d2fb0
Danilo Bargen <mail@dbrgn.ch>
2016-07-02 12:44:05 +0200

    Deserialize measurement dates

--- app/src/main/java/ch/coredump/watertemp/rest/models/Measurement.java
+++ app/src/main/java/ch/coredump/watertemp/rest/models/Measurement.java
@@ -1,14 +1,20 @@
 package ch.coredump.watertemp.rest.models;
 
+import java.util.Date;
+
 public class Measurement {
     private final int id;
     private final int sensorId;
     private final float temperature;
+    private final Date createdAt;
+    private final Date updatedAt;
 
-    public Measurement(int id, int sensorId, float temperature) {
+    public Measurement(int id, int sensorId, float temperature, Date createdAt, Date updatedAt) {
         this.id = id;
         this.sensorId = sensorId;
         this.temperature = temperature;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
     public int getId() {
@@ -21,5 +27,13 @@ public class Measurement {
 
     public float getTemperature() {
         return temperature;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
     }
 }
