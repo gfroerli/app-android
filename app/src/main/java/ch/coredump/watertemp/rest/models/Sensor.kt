8fdbd4855e88c32f4980a6e86ad715390ca521e5
Danilo Bargen <mail@dbrgn.ch>
2017-11-08 01:25:08 +0100

    Convert Sensor POJO to a Kotlin data class

---
+++ app/src/main/java/ch/coredump/watertemp/rest/models/Sensor.kt
@@ -0,0 +1,12 @@
+package ch.coredump.watertemp.rest.models
+
+/**
+ * Gson Sensor model.
+ */
+data class Sensor(
+    val id: Int,
+    val deviceName: String,
+    val caption: String,
+    val latitude: Double,
+    val longitude: Double
+)
