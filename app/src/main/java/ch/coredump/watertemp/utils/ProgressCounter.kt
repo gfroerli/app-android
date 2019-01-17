340c948b7a83865f42b84f6e3d25eed1af545a4a
Danilo Bargen <mail@dbrgn.ch>
2019-01-17 21:34:22 +0100

    Add loading indicator

---
+++ app/src/main/java/ch/coredump/watertemp/utils/ProgressCounter.kt
@@ -0,0 +1,38 @@
+package ch.coredump.watertemp.utils
+
+import android.view.View
+
+/**
+ * Track multiple progress events.
+ *
+ * Show the progress view when the first event is started,
+ * hide it when the last event has stopped.
+ */
+class ProgressCounter(val progressView: View) {
+    var count = 0
+
+    /**
+     * Start a progress activitiy.
+     */
+    @Synchronized
+    fun start() {
+        count += 1
+        if (count == 1) {
+            this.progressView.visibility = View.VISIBLE
+        }
+    }
+
+    /**
+     * Stop a progress activitiy.
+     */
+    @Synchronized
+    fun stop() {
+        if (count == 0) {
+            return;
+        }
+        count -= 1
+        if (count == 0) {
+            this.progressView.visibility = View.INVISIBLE
+        }
+    }
+}
