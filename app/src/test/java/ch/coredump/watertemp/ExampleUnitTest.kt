3c8c8c23438f98d6853e5fa0a33d02e33205be1e
Danilo Bargen <mail@dbrgn.ch>
2020-09-05 01:46:37 +0200

    Set up unit tests

---
+++ app/src/test/java/ch/coredump/watertemp/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package ch.coredump.watertemp
+
+import org.junit.Assert
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun testAddition() {
+        Assert.assertEquals(4, 2 + 2.toLong())
+    }
+}
