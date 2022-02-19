f3581d8fded38cc0a36dcc30ccbffea518127407
Danilo Bargen <mail@dbrgn.ch>
2022-02-20 00:00:24 +0100

    Fix instrumentation tests
    See https://stackoverflow.com/a/71189923/284318

--- app/src/main/java/ch/coredump/watertemp/utils/ProgressCounter.kt
+++ app/src/main/java/ch/coredump/watertemp/utils/ProgressCounter.kt
@@ -2,6 +2,7 @@ package ch.coredump.watertemp.utils
 
 import android.util.Log
 import android.view.View
+import androidx.compose.runtime.NoLiveLiterals
 import com.google.android.material.progressindicator.LinearProgressIndicator
 
 private const val TAG = "ProgressCounter"
@@ -12,8 +13,9 @@ private const val TAG = "ProgressCounter"
  * Show the progress view when the first event is started,
  * hide it when the last event has stopped.
  */
+@NoLiveLiterals
 class ProgressCounter(private val progressView: LinearProgressIndicator) {
-    var count = 0
+    var count: Int = 0
 
     /**
      * Start a progress activitiy.
