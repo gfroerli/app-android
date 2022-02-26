76dfeb77d41dcf27977830eac19c94522ef90286
Danilo Bargen <mail@dbrgn.ch>
2022-02-27 00:37:47 +0100

    Add test for about activity (#51)

--- app/src/androidTest/java/ch/coredump/watertemp/MapActivityTest.kt
+++ app/src/androidTest/java/ch/coredump/watertemp/MapActivityTest.kt
@@ -1,8 +1,8 @@
 package ch.coredump.watertemp
 
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithText
 import ch.coredump.watertemp.activities.MapActivity
 import org.junit.Rule
 import org.junit.Test
@@ -16,5 +16,17 @@ class MapActivityTest {
     fun testTitle() {
         val title = composeTestRule.activity.getString(R.string.activity_map)
         composeTestRule.onNodeWithText(title).assertExists("Title text not found")
+    }
+
+    @Test
+    fun testAbout() {
+        // Tap on menu button
+        composeTestRule.onNodeWithContentDescription("More").performClick()
+
+        // Pick "about" entry
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.action_about_this_app)).performClick()
+
+        // Ensure about activity is opened
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.activity_about)).assertExists("Title text not found")
     }
 }
