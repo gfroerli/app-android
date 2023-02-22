a1ab506f323ead067a3082f95e521357962f2d68
Danilo Bargen <mail@dbrgn.ch>
2023-02-22 23:34:38 +0100

    Remove unused helper function

--- app/src/main/java/ch/coredump/watertemp/Utils.java
+++ app/src/main/java/ch/coredump/watertemp/Utils.java
@@ -2,30 +2,10 @@ package ch.coredump.watertemp;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+
 import androidx.appcompat.app.AlertDialog;
 
-import java.util.List;
-
 public class Utils {
-
-    /**
-     * Join a string list using a delimiter.
-     * @param delimiter The delimiter.
-     * @param elements List of string elements.
-     * @return The joined string.
-     */
-    public static String join(String delimiter,
-                              List<String> elements) {
-        StringBuilder builder = new StringBuilder();
-        final int size = elements.size();
-        for (int i = 0; i < size - 1; i++) {
-            builder.append(elements.get(i));
-            builder.append(delimiter);
-        }
-        builder.append(elements.get(size - 1));
-        return builder.toString();
-    }
-
     /**
      * Show an error message in a simple dialog.
      * @param activity The activity context.
